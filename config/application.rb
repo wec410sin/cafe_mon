22b61633fa88e73ceabc22a96735d550fa1e615e
wec410sin <wec410sin@yahoo.co.jp>
2021-06-07 18:23:11 +0900

    検証を無効化

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module CafeMon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
-        config.time_zone = 'Asia/Tokyo'
+    config.time_zone = 'Asia/Tokyo'
+    config.action_controller.allow_forgery_protection = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
